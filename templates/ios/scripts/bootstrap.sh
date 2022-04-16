f905b4d8c5fca6b86d9373bf3c42dd678c3ab560
shion <tenta_shion@yahoo.co.jp>
2022-04-16 18:50:42 +0900

    ios テンプレートの仮実装

---
+++ templates/ios/scripts/bootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# シェルの確認
+if [ "$SHELL" = '/bin/bash' ] || [ "$SHELL" = '/usr/local/bin/bash' ]; then
+    SHELL_RESOURCE=~/.bashrc
+elif [ "$SHELL" = '/bin/zsh' ] || [ "$SHELL" = '/usr/local/bin/zsh' ]; then
+    SHELL_RESOURCE=~/.zshrc
+else
+    echo "Unsupported shell: ${SHELL}"
+    exit 1
+fi
+
+
+# 必須コマンドの確認
+if ! type "git" > /dev/null; then
+    echo '`git` not found.'
+    exit 1
+fi
+if ! type "mint" > /dev/null; then
+    echo '`mint` not found.'
+    exit 1
+fi
+
+
+# Mintfile からツール依存関係を復元
+mint bootstrap
